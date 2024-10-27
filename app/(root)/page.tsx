b05ebba25739035fe26a4b3c31269819c0cf60e4
PallaviKumari <pallavi4757kumari@gmail.com>
2024-10-27 11:52:35 +0530

    add some functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 const Home = () => {
+    const loggedIn = {firstName: 'Pallavi', lastname:'kumari', email:'pk.23cs8067@nitdgp.ac.in'}
   return (
     <>
-    <h1> Home </h1>
+   <section className="home">
+<div className="home-content">
+    <header className="home-header">
+      <HeaderBox 
+      type = "greeting"
+      title = "Welcome"
+      user = {loggedIn?.firstName || 'Guest'}
+      subtext = "Access and manage your account transaction"
+
+      />
+      <TotalBalanceBox 
+      accounts = {[]}
+      totalBanks = {1}
+      totalCurrentBalance = {1250}
+      />
+    </header>
+    RECENT TRANSACTION
+</div>
+<RightSidebar 
+  user = {loggedIn}
+  transactions = {[]}
+  banks = {[{currentBalance:3456}, {currentBalance:3456}]}
+  />
+   </section>
     </>
   )
 }
